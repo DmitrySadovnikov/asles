679e23adf64882d019b7a8bcda010d9d2d3b7c9f
Dmitry Sadovnikov <d.sadovnikov@yahoo.com>
2016-05-17 19:46:24 +0300

    rollback

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,10 @@
+class StaticController < ApplicationController
+  def index
+  end
+  def catalog
+    end
+  def services
+  end
+  def contacts
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  root 'static#index'
+  get '/index' => 'static#index'
+  get '/catalog' => 'static#catalog'
+  get '/services' => 'static#services'
+  get '/contacts' => 'static#contacts'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end

71ba0fbafb9586776c857d44d291eb0f48c5135d
Dmitry Sadovnikov <d.sadovnikov@yahoo.com>
2016-05-18 17:56:27 +0300

    add catalog and admin pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   resources :products
   root 'static#index'
   get '/index' => 'static#index'
-  get '/catalog' => 'static#catalog'
+  get '/catalog' => 'products#catalog'
   get '/services' => 'static#services'
   get '/contacts' => 'static#contacts'
+  get '/admin' => 'products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

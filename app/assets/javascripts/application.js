ae8602363625e9df1fa6d44a625cd20cc548a917
Dmitry Sadovnikov <d.sadovnikov@yahoo.com>
2016-05-20 23:13:57 +0300

    update table from show2 page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,68 @@
 //= require turbolinks
 //= require bootstrap-sprockets
 //= require_tree .
+
+$(document).ready(function(){
+
+    $(".sortable-table th").click(function(){
+        sort_table($(this));
+    });
+
+});
+
+function sort_table(clicked){
+    var current_table = clicked.parents(".sortable-table"),
+        sorted_column = clicked.index(),
+        column_count = current_table.find("th").length,
+        sort_it = [];
+
+    current_table.find("tbody tr").each(function(){
+        var new_line = "",
+            sort_by = "";
+        $(this).find("td").each(function(){
+            if($(this).next().length){
+                new_line += $(this).html() + "+";
+            }else{
+                new_line += $(this).html();
+            }
+            if($(this).index() == sorted_column){
+                sort_by = $(this).text();
+            }
+        });
+
+        new_line = sort_by + "*" + new_line;
+        sort_it.push(new_line);
+        //console.log(sort_it);
+
+    });
+
+    sort_it.sort();
+    $("th span").text("");
+    if(!clicked.hasClass("sort-down")){
+        clicked.addClass("sort-down");
+        clicked.find("span").text("▼");
+    }else{
+        sort_it.reverse();
+        clicked.removeClass("sort-down");
+        clicked.find("span").text("▲");
+    }
+
+    $("#country-list tr:not('.country-table-head')").each(function(){
+        $(this).remove();
+    });
+
+    $(sort_it).each(function(index, value) {
+        $('<tr class="current-tr"></tr>').appendTo(clicked.parents("table").find("tbody"));
+        var split_line = value.split("*"),
+            td_line = split_line[1].split("+"),
+            td_add = "";
+
+        //console.log(td_line.length);
+        for (var i = 0; i < td_line.length; i++){
+            td_add += "<td>" + td_line[i] + "</td>";
+        }
+        $(td_add).appendTo(".current-tr");
+        $(".current-tr").removeClass("current-tr");
+
+    });
+}

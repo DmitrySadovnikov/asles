0304da3689dae6f74ef54c89b18c25e72caf0da8
Dmitry Sadovnikov <d.sadovnikov@yahoo.com>
2016-05-29 21:11:32 +0300

    modify catalog table

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,16 @@
 module ApplicationHelper
-  def sortable(column, title = nil)
+  def sortable(column, title)
     title ||= column.titleize
     css_class = column == sort_column ? "current #{sort_direction}" : nil
     direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
     link_to title, {:sort => column, :direction => direction}, {:class => css_class}
   end
+
+  def sortable_catalog(column, title, current_page)
+    title ||= column.titleize
+    css_class = column == sort_column ? "current #{sort_direction}" : nil
+    direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
+    link_to title, '/catalog/'+ current_page +'?direction='+ direction +'&sort=' + column, {:class => css_class}
+  end
+
 end

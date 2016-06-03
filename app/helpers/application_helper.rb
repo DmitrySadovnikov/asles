b1c844a5e32660626985464e3b8f919e3e98c2e2
Dmitry Sadovnikov <d.sadovnikov@yahoo.com>
2016-06-03 23:36:14 +0300

    fix catalog

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,7 +10,19 @@ module ApplicationHelper
     title ||= column.titleize
     css_class = column == sort_column ? "current #{sort_direction}" : nil
     direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
-    link_to title, '/catalog/'+ current_page +'?direction='+ direction +'&sort=' + column, {:class => css_class}
+    link_to title, '/catalog/'+ my_gsub_in(current_page) +'?direction='+ direction +'&sort=' + column, {:class => css_class}
+  end
+
+  def list
+    Product.select("DISTINCT(name)").order("name")
+  end
+
+  def my_gsub_in (string)
+    string.gsub("/", "_").gsub("\\", "_").gsub("(", "-+").gsub(")", "+-")
+  end
+
+  def my_gsub_out (string)
+    string.gsub("_", "/").gsub("-+", "(").gsub("+-", ")")
   end
 
 end
